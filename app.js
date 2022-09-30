ee03c71780bc4529f02b3ca30afe4209426f5308
anthpantaleo <anthpantaleo@gmail.com>
2022-09-29 22:49:07 -0400

    some structure, going to figure out modal tomorrow

---
+++ app.js
@@ -0,0 +1,11 @@
+const bookButton = document.querySelector(".addbook");
+
+let myLibrary = [];
+
+function Book() {}
+
+function addBookToLibrary() {}
+
+bookButton.addEventListener("click", function (e) {
+  console.log(e);
+});
